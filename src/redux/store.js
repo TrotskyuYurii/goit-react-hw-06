e0acba0bf66f1ae3cf8181a44478a8965baf529a
TrotskyuYurii <tro2001@gmail.com>
2024-04-13 20:51:51 +0300

    store_toolKit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,10 @@
-import { createStore, combineReducers } from "redux";
-import { devToolsEnhancer } from "@redux-devtools/extension";
+import { configureStore } from "@reduxjs/toolkit";
 import { contactsSlice } from "./contactsSlice";
 import { filtersSlice } from "./filtersSlice";
 
-
-const rootReducer = combineReducers({
-  contacts: contactsSlice,
-  filter: filtersSlice
-})
-
-const enhancer = devToolsEnhancer();
-
-export const store = createStore(rootReducer, enhancer);
+export const store = configureStore({
+  reducer: {
+    contacts: contactsSlice,
+    filter: filtersSlice
+  },
+});
