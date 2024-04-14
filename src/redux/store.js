8ee945d5f9ba546640b9f1c4a6b5072ed646e778
TrotskyuYurii <tro2001@gmail.com>
2024-04-14 10:23:09 +0300

    disk_3

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import { contactsReducer } from "./contactsSlice";
 import { filtersReducer } from "./filtersSlice";
 
-//persist - імопртуємо необхідні бібліотеки persist redux
+//persist - імопртуємо необхідні бібліотеки persist redux. З цим не розбирємось - просто копіюємо
 import {
   persistStore,
   persistReducer,
@@ -20,17 +20,17 @@ import storage from "redux-persist/lib/storage";
 const contactsPeristConfig = {
   key: "contactsitems",
   storage,
-  whitelist: ["items"],
+  whitelist: ["items"], // зберігаємо лише властивість items з головного сховища у локальному сховищі (білий список)
 };
 //persist
 
 export const store = configureStore({
   reducer: {
-    contacts: persistReducer(contactsPeristConfig, contactsReducer), //redux-persist - додаємо збережені дані в локальному сховищі
+    contacts: persistReducer(contactsPeristConfig, contactsReducer), //redux-persist - додаємо до головного сховища збережені дані в локальному сховищі
     filters: filtersReducer
   },
 
-  //persist - конфігурація middleware для redux-persist
+  //persist - конфігурація middleware для redux-persist. З цим не розбирємось - просто копіюємо
   middleware: (getDefaultMiddleware) =>
   getDefaultMiddleware({
     serializableCheck: {
